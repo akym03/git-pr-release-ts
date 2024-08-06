2e7142796ff4457ac571cfc1ef16a9e6e2997470
masahiro akiyama <akiyama@bk.iij4u.or.jp>
2024-08-06 16:54:59 +0900

    fix: 最低限のコンパイルエラーを修正

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import { Probot } from "probot";
+import {Context}  from 'probot/lib';
 
 export default (app: Probot) => {
-  app.on("issues.opened", async (context) => {
+  app.on("issues.opened", async (context: Context) => {
     const issueComment = context.issue({
       body: "Thanks for opening this issue!",
     });
