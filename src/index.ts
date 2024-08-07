59a2f1ccd81a84471a9368b157267157ca28e691
masahiro akiyama <akiyama@bk.iij4u.or.jp>
2024-08-07 13:16:36 +0900

    chore: 処理概要を記載

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,17 @@ import { Probot } from "probot";
 import {Context}  from 'probot/lib';
 
 export default (app: Probot) => {
-  app.on("issues.opened", async (context: Context) => {
-    const issueComment = context.issue({
-      body: "Thanks for opening this issue!",
-    });
-    await context.octokit.issues.createComment(issueComment);
+  app.on("push", async (_context: Context) => {
+
+    // TODO checking arguments
+    // https://github.com/x-motemen/git-pr-release/blob/4c547de55eeb65a068b5fd1ab2be93f64f3fc061/lib/git/pr/release/cli.rb#L24
+
+    // TODO Set up configuration
+    // https://github.com/x-motemen/git-pr-release/blob/4c547de55eeb65a068b5fd1ab2be93f64f3fc061/lib/git/pr/release/cli.rb#L42C11-L42C35
+
+    // TODO Create or update release PR
+    // https://github.com/x-motemen/git-pr-release/blob/4c547de55eeb65a068b5fd1ab2be93f64f3fc061/lib/git/pr/release/cli.rb#L52C15-L53C1
+
   });
   // For more information on building apps:
   // https://probot.github.io/docs/
